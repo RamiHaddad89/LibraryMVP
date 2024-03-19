78cab07c06d5145962914144c4cf931973b5cbca
Rami Haddad <ramihaddad8989@live.com>
2024-03-19 22:45:36 +0300

    add attributes

--- LibraryMVP/Program.cs
+++ LibraryMVP/Program.cs
@@ -16,7 +16,7 @@ namespace LibraryMVP
 		{
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new Form1());
+			Application.Run(new Views.F_Main());
 		}
 	}
 }
